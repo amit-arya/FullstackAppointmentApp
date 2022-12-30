7140338d60333b75852c52ea36a307ceeb482428
amit-arya <amit3128arya@gmail.com>
2022-12-31 01:06:10 +0530

    Add files via upload

--- admin.js
+++ admin.js
@@ -1,21 +1,72 @@
-const path = require('path');
+const User = require("../models/user");
 
-const express = require('express');
+exports.postAddUser = (req, res, next) => {
+  const username = req.body.username;
+  const phone = req.body.phone;
+  const email = req.body.email;
+  const newuser = User.create({
+    username: username,
+    phone: phone,
+    email: email
+  })
+    .then(result => {
+      console.log('Created User');
+      res.redirect('/');
+    })
+    .catch(err => {
+      console.log(err);
+    })
+};
 
-const adminController = require('../controllers/admin');
+exports.getEditUser = (req, res, next) => {
+  const editMode = req.query.edit;
+  if(!editMode){
+    return res.redirect('/users');
+  }
+  const userId = req.params.userId;
+  User.findByPk(userId)
+  .then(user => {
+    if (!user) {
+      return res.redirect("/users");
+    }
+    res.render("admin/edit-user", {
+      pageTitle: "Edit User",
+      path: "/edit-user",
+      editing: editMode,
+      user: user,
+    });
+  })
+  .catch(err => console.log(err));
+};
 
-const router = express.Router();
+exports.postEditUser = (req, res, next) => {
+  const userId = req.body.userId;
+  const updatedUsername = req.body.username;
+  const updatedPhone = req.body.phone;
+  const updatedEmail = req.body.email;
+  User.findByPk(userId)
+  .then(user => {
+    user.username = updatedUsername;
+    user.phone = updatedPhone;
+    user.email = updatedEmail;
+    return user.save();
+  })
+  .then(result => {
+    console.log('UPDATED PRODUCT!');
+    res.redirect('/users');
+  })
+  .catch(err => console.log(err));
+};
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'user.html'));
-});
-
-router.post('/', adminController.postAddUser);
-
-router.get('/edit-user/:userId', adminController.getEditUser);
-
-router.post('/edit-user', adminController.postEditUser);
-
-router.post('/delete-user',adminController.postDeleteUser);
-
-module.exports = router;
+exports.postDeleteUser = (req, res, next) => {
+  const userId = req.body.userId;
+  User.findByPk(userId)
+    .then(user => {
+      return user.destroy();
+    })
+    .then(result => {
+      console.log('DESTROYED PRODUCT');
+      res.redirect('/users');
+    })
+    .catch(err => console.log(err));
+};
